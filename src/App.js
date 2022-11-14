de0d3cdc9f89de5911fd6025729e15d60fd2ff04
learnReactgithub <sonukushwah362@gmail.com>
2022-11-14 16:35:18 +0530

    useEffect with specific state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [count1,setCount1]=useState(1);
+  const [count2,setCount2]=useState(10);
+
+  useEffect(()=>{
+    console.log("useEffect Called in Component Mount");
+  },[count1])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h3>UseEffect Hooks with specific props</h3>
+     <h4>Count 1:{count1}</h4>
+     <h4>Count 2:{count2}</h4>
+     <button onClick={()=>setCount1(count1+1)}>Couner 1</button>
+     <button onClick={()=>setCount2(count2+2)}>Counter 2</button>
+    </>
   );
 }
 
